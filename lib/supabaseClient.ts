9af024c7982a2d28cff882928ce9c4bdb78cf5cc
Jonny <jonny.dunk52@gmail.com>
2025-08-28 08:21:03 -0700

    feat: Upgrade Supabase to use @supabase/ssr

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,8 +1,10 @@
-import { createClient } from '@supabase/supabase-js';
+// lib/supabaseClient.ts
+
+import { createBrowserClient } from '@supabase/ssr'
 import { envClient } from './env.client';
 
-// Create the client a single time and export the instance
-export const supabase = createClient(
+// The new, recommended way to create a browser client for Next.js App Router
+export const supabase = createBrowserClient(
   envClient.NEXT_PUBLIC_SUPABASE_URL,
   envClient.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );

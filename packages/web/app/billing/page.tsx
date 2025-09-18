be0d534d038212a5646b86358ca98c148302d32e
Jonny <jonny.dunk52@gmail.com>
2025-09-18 10:39:23 -0700

    chore: remove legacy pages

--- packages/web/app/billing/page.tsx
+++
@@ -1,9 +0,0 @@
-import BillingPanel from '@/components/billing/BillingPanel';
-
-export default function BillingPage() {
-  return (
-    <main className="min-h-screen bg-[#0b0b12] text-white">
-      <BillingPanel variant="page" />
-    </main>
-  );
-}
